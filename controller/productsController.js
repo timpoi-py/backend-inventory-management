c9549373d70a3e910719698ed5b3b5ad94467a1d
Timothy Paul Ignacio <timothypaulignacio@gmail.com>
2022-11-30 20:09:44 +0800

    Controller for Products is done

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,6 +1,7 @@
 const Products = require("../models/Products");
 const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
+const { deleteModel } = require("mongoose");
 
 // @desc Get all products
 // @route GET /products
@@ -26,7 +27,7 @@ const createProduct = asyncHandler(async (req, res) => {
   }
 
   // Check for duplicates
-  const duplicate = await Products.findOne({ name, size }).lean().exec();
+  const duplicate = await Products.findOne({ name, size, brand }).lean().exec();
   if (duplicate) {
     return res
       .status(400)
@@ -42,20 +43,75 @@ const createProduct = asyncHandler(async (req, res) => {
     price,
   };
 
-  const productDoc = new Products(createProductItem);
+  const product = await Products.create(createProductItem);
 
-  await productDoc.save();
+  if (product) {
+    res
+      .status(201)
+      .json({ message: `Product ${name} with size ${size} is created` });
+  } else {
+    res.status(400).json({ message: "Invalid credentials" });
+  }
 });
 
 // @desc Update a product
 // @route PATCH /product
 // @access Private
-const updateProduct = asyncHandler(async (req, res) => {});
+const updateProduct = asyncHandler(async (req, res) => {
+  // Get data in request body
+  const { id, name, size, unit, type, brand, price } = req.body;
+
+  // validate data
+  if (!id || !name || !size || !unit || !type || !brand || !price) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // get product
+  const product = await Products.findById(id).exec();
+  if (!product) {
+    res.status(400).json({ message: "Product not found" });
+  }
+
+  // check duplicate
+  const duplicate = await Products.findOne({ name, brand, size }).lean().exec();
+  if (duplicate && duplicate?._id.toString() !== id) {
+    res.status(409).json({ message: "Error: Duplicate products" });
+  }
+
+  product.name = name;
+  product.size = size;
+  product.unit = unit;
+  product.type = type;
+  product.brand = brand;
+  product.price = price;
+
+  const updatedProduct = await product.save();
+
+  res.json({ message: `${updatedProduct.name} has been updated` });
+});
 
 // @desc Delete a product
 // @route DELETE /products
 // @access Private
-const deleteProduct = asyncHandler(async (req, res) => {});
+const deleteProduct = asyncHandler(async (req, res) => {
+  // get id from the request body
+  const { id } = req.body;
+  if (!id) {
+    res.status(400).json({ message: "Product ID is required" });
+  }
+
+  // Find the product by ID
+  const product = await Products.findById(id).exec();
+  if (!product) {
+    res.status(400).json({ message: "Product not found" });
+  }
+
+  const deletedProduct = await product.deleteOne();
+
+  res.json({
+    message: `${deletedProduct.name} with brand ${deletedProduct.brand} and with a size of ${deletedProduct.size} ${deletedProduct.unit} has been deleted`,
+  });
+});
 
 module.exports = {
   getAllProducts,
